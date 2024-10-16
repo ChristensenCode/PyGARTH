e42cb0066568d4c82b9a4c4ca722979a417efe4f
ChristensenCode <ryan.john.christensen12@gmail.com>
2024-10-15 19:58:47 -0700

    added the logging config and removed placeholder x

--- pygarth/__main__.py
+++ pygarth/__main__.py
@@ -3,12 +3,12 @@ from pygarth.config_logger import logging_config
 import logging
 
 logger = logging.getLogger(__name__)
+logging_config()
 
 
 def main():
     print("Hello PyGARTH! ")
     input_args = cli()
-    x = 1
 
 
 if __name__ == "__main__":
